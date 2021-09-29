cece0c2112edb7abea93a24f0aefa5ab1d988af5
jimmyhu <60071jimmy@gmail.com>
2021-09-29 11:36:23 +0800

    Update GenerateNoiseImages.m
    Update GenerateNoiseImages.m

--- src/generate_testing_sample/GenerateNoiseImages.m
+++ src/generate_testing_sample/GenerateNoiseImages.m
@@ -1,3 +1,9 @@
+%   For Octave
+%   pkg install -forge image
+%   pkg load image
+
+%%  Visiting each sub-folder
+
 for outer_loop = 1:1
     for inner_loop = 1:1
         input_path = [input_root filesep sprintf('%05d', outer_loop) filesep sprintf('%04d', inner_loop)];
@@ -5,6 +11,7 @@ for outer_loop = 1:1
             continue;
         end
         output_path = ['.' filesep 'Output' filesep sprintf('%05d', outer_loop) filesep sprintf('%04d', inner_loop)];
-        add_noise_to_input(output_path, output_path);
+        add_noise_to_input(input_path, output_path);
+        fprintf("Processing for folder %s finished!\n", output_path);
     end
 end
